f070a7001d89981d87687b39ee550e5b94fae146
Ryan Chang Jia Jie <>
2022-09-13 14:28:03 +0800

    Merge branch 'jm_frontend' into branch-ryan-sprint-2

--- user-service/index.js
+++ user-service/index.js
@@ -19,4 +19,4 @@ app.use('/api/user', router).all((_, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
 })
 
-app.listen(8000, () => console.log('user-service listening on port 8000'));
+app.listen(8000, () => console.log('user-service listening on port 8000'));
