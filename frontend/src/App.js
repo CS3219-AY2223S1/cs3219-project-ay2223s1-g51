63f9cfe88cbd383b93dbc9b23ba0b018c1fa26ba
SimJM <SimJM@users.noreply.github.com>
2022-10-11 23:10:07 +0800

    added navbar and profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import {Box} from "@mui/material";
 import SignupPage from './components/SignupPage';
 import Login from './components/Login';
-import {Box} from "@mui/material";
 import HomePage from "./components/HomePage";
+import ProfilePage from './components/ProfilePage';
+import NavBar from "./components/NavBar";
 
 function App(props) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     return (
         <div className="App">
+            <NavBar username={username} password={password} setUsername={setUsername} setPassword={setPassword} ></NavBar>
             <Box display={"flex"} flexDirection={"column"} padding={"4rem"}>
                 <Router>
                     <Routes>
                         <Route exact path="/" element={<Navigate replace to="/login" />}></Route>
-                        <Route path="/login" element={<Login username={username} password={password} setUsername={setUsername} setPassword={setPassword}/>}/>
+                        <Route path="/login" element={<Login username={username} password={password} setUsername={setUsername} setPassword={setPassword} />}/>
                         <Route path="/signup" element={<SignupPage/>}/>
                         <Route path="/home" element={<HomePage username={username} password={password} setPassword={setPassword}/>}/>
+                        <Route path="/profile" element={<ProfilePage username={username}  password={password} setPassword={setPassword}/>}/>
                     </Routes>
                 </Router>
             </Box>
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -3,20 +3,8 @@ import {
     Button,
     Container,
     CssBaseline,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
-    Grid,
-    Link,
-    TextField,
     Typography,
 } from "@mui/material";
-import axios from "axios";
-import { URL_USER_DELETE_SVC, URL_USER_EDITPASSWORD_SVC } from "../configs";
-import { STATUS_CODE_DATABASE_ERROR, STATUS_CODE_SUCCESS } from "../constants";
-import { useState } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles"
 
 const themeLight = createTheme({
@@ -28,67 +16,6 @@ const themeLight = createTheme({
 });
 
 function HomePage(props) {
-    const [isDialogOpen, setIsDialogOpen] = useState(false);
-    const [dialogTitle, setDialogTitle] = useState("");
-    const [dialogMsg, setDialogMsg] = useState("");
-    const [isDelete, setIsDelete] = useState(false);
-    const [changedPassword, setChangedPassword] = useState("");
-
-
-    //hardcoded for now, need to change to dynamic later
-    const handleDelete = async () => {
-        const res = await axios.delete(URL_USER_DELETE_SVC + props.username).catch((err) => {
-            if (err.response.status === STATUS_CODE_DATABASE_ERROR) {
-                setErrorDialog("Server error, Please try again later.");
-            } else {
-                setErrorDialog("Please try again later.");
-            }
-        });
-
-        if (res && res.status === STATUS_CODE_SUCCESS) {
-            setSuccessDialog("Account successfully deleted");
-            setIsDelete(true);
-        }
-    };
-
-    //hardcoded for now, need to change to dynamic later
-    const handleChangePassword = async () => {
-        const res = await axios
-            .put(URL_USER_EDITPASSWORD_SVC, {
-                username: props.username,
-                password: changedPassword,
-            })
-            .catch((err) => {
-                if (err.response.status === STATUS_CODE_DATABASE_ERROR) {
-                    setErrorDialog("Server error, Please try again later.");
-                } else {
-                    setErrorDialog("Please try again later.");
-                }
-            });
-        if (res && res.status === STATUS_CODE_SUCCESS) {
-            setSuccessDialog("Password successfully changed");
-        }
-    };
-
-    const passwordHandler = (e) => {
-		var userNamePassword = props.username + e.target.value
-        var md5Hash = require("md5-hash")
-        var saltedPassword = md5Hash.default(userNamePassword)
-		props.setPassword(saltedPassword)
-		setChangedPassword(saltedPassword)
-	}
-
-    const setErrorDialog = (msg) => {
-        setIsDialogOpen(true);
-        setDialogTitle("Error");
-        setDialogMsg(msg);
-    };
-
-    const setSuccessDialog = (msg) => {
-        setIsDialogOpen(true);
-        setDialogTitle("Success");
-        setDialogMsg(msg);
-    };
 
     const openPeerPrepRoomHandler = () => {
         window.location.href = 'http://localhost:8001/index.html'
@@ -100,63 +27,37 @@ function HomePage(props) {
                 <Container component="main" maxWidth="xs">
                     <CssBaseline />
                     <Box
-                    sx={{
-                        marginTop: 8,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                    >
-                    <Typography component="h1" variant="h5">
-                        PeerPrep
-                    </Typography>
-                    <Box
-                        component="form"
-                        // onSubmit={handleLogin}
-                        noValidate
                         sx={{
-                            marginTop: 2,
+                            marginTop: 8,
                             display: "flex",
                             flexDirection: "column",
                             alignItems: "center",
                         }}
                     >
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            onClick={openPeerPrepRoomHandler}
+                    <Typography component="h1" variant="h5">
+                        Welcome to PeerPrep!
+                    </Typography>
+                        <Box
+                            component="form"
+                            // onSubmit={handleLogin}
+                            noValidate
+                            sx={{
+                                marginTop: 2,
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                            }}
                         >
-                            Start PeerPrep !
-                        </Button>
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            onClick={handleDelete}
-                            href="/login"
-                        >
-                            Delete Account
-                        </Button>
-                        <TextField
-                            fullWidth
-                            autoFocus
-                            label="New Password"
-                            variant="standard"
-                            type="password"
-                            onChange={passwordHandler}
-                        />
-                        <Button onClick={handleChangePassword}>Change Password</Button>
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            href="/login"
-                        >
-                            Logout
-                        </Button>
-                        <div style={{width:"400px"}}></div>
-                    </Box>
+                            <Button
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                                onClick={openPeerPrepRoomHandler}
+                            >
+                                Start PeerPrep
+                            </Button>
+                            <div style={{width:"400px"}}></div>
+                        </Box>
                     </Box>
                 </Container>
             </ThemeProvider>
