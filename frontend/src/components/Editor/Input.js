43b4184f937846b5f384d338acdf21bad6d857cf
Zong Yu <internityz123@gmail.com>
2022-10-20 03:34:42 +0800

    Fix bugs

--- frontend/src/components/Editor/Input.js
+++ frontend/src/components/Editor/Input.js
@@ -2,23 +2,23 @@ import React from "react";
 import Editor from "@monaco-editor/react";
 
 export default function Input(props) {
+  const { setProperty, value, theme, fontSize, feature } = props;
   const handleEditorChange = (value, event) => {
-    console.log("here is the current model value:", value);
-    props.setProperty(value);
+    setProperty(value);
   };
   return (
     <>
-      <div className={props.theme === "vs-dark" ? "text-center bg-light" : "text-center bg-dark text-light"}>
-        <strong>{props.feature}</strong>
+      <div className={theme === "vs-dark" ? "text-center bg-light" : "text-center bg-dark text-light"}>
+        <strong>{feature}</strong>
       </div>
       <section>
         <Editor
           defaultLanguage="plaintext"
           height="21vh"
-          theme={props.theme}
-          defaultValue=""
+          theme={theme}
+          value={value}
           onChange={handleEditorChange}
-          options={{ fontSize: props.fontSize }}
+          options={{ fontSize: fontSize }}
         />
       </section>
     </>
