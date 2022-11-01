b34a339cbb7f27f419a5ba2f75178ac3be3a694d
Ryan Chang Jia Jie <>
2022-11-02 00:51:42 +0800

    add serviceAccount.js

---
+++ user-service/serviceAccount.js
@@ -0,0 +1,12 @@
+export const serviceAccount = {
+  "type": "service_account",
+  "project_id": "cs3219-8da08",
+  "private_key_id": "1d21714e0cb4e3f22d2fbd3c49cac4879414725d",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCwE5wvRrUk5EDq\nAfQpfJuiJqvvsCLyOv8BhIf36fwej/3mgI8m+qMLZO2fjm71i+Sapzt/C58EqMGL\nm7fRiGwqpGYVTHzDJdbEbtOnPRotPQwaAHDF0e1//D91SqT38/dfYodVGAV5KK7K\nzvScs+bXpiHMqlUDR6cOXW4DkRH6F0rIGM7dMT+w4OqTdDqqxmjTgPEFUbBWbmzQ\nqUHDaWfIwg0wXZmXJOt/a0B19I/VceN2MCN50BoxKONd+BMXrjUH8Ucr60k2a1vv\ncRau6BLF0JLZga5kv2SQmDLK7Tl+haNpVmqkHHGNLaGBsP/rp3GGj3rCGndVlYaH\nd+o/IWbFAgMBAAECggEAFloO8CNyOGIvaWVhGOkkGqP9HJrH/OcHWyp3Vj3iTwDp\nctrsET8s9nPUBHbA2DPvi8Bh+aFAiQdhNndIkPVYAdvHx7ljdyR1DI+jF65Whdqj\nkCNxLXOWSjJ73chhg8Z5JycrVd6MR1KmScGW5NOjoMmKreALlbSBXP34SsSeOxiG\nzveApYg6YwwRD5zEdQRfze2mIjEeHouydChffPMkfazhQLcPfokQ0RZb8OUSR5H2\nQvaZElFjxEu5eKLTxi+TCuNzJC0J3hHhYJnpU4pvdKptYviToPALLPHrfLv4WAEc\nA/cRiPCpuC1+YuX46FX4Ow76p63bcTtz8iQRDsDcsQKBgQDkMbii5RUB6eFA7RcV\nbW3rGSKYGoWR8ZvTIjisIQ4x1rJrdV/rY6LN/FEudgA5wcY8fJsxuz8TLB/wlqsc\n+KjZB8SE3wtTvKPUysUCC91XQFykeovRPw/HwoetDAGn889yi+pOOf+oAytcTOYc\nYRG2Rvt+/IJpuizMe60mniNeLQKBgQDFiCJtFGh+moFQNU0A/3hgHe4H7r5in6mg\n7rXkUkFRlWXes03NStyWOOw4dLMUrxZ6rqiXMA0rtV5wBWcijeDfjFZcTkEGYY4Q\nhXQoCh3y+6nfCOEi74IZPlhj8GLRxvpwxjAD2+Hm4lyJC10WlMItn6OP/cQk9aYE\narhvE1Mh+QKBgBGsmDRlsd7YEo5IzsTmGOFgrQFC7J2FLSCJQgJy/Pp3OpKAINHG\ngdGafV8v3IycqZH9xLdxXU1K2Z50zQQNbmaA54OzEtmFftr4aZHjAV3EqmhC5Zlr\nXPmH46JPj8FGbQbeN2OfsmF8/VrEyf3gttJ7ZPdM73zo4pdj7OFU1kBxAoGAG4b+\nLS9T5JjJfpD++y2e694f5tEZjQrtC7VYecoQujrUqHVaWUqedPzXxoJU1oQT4OLo\nMbGLlFTBJWFLXyBgfybCfjiVjTsZHipK5q2KQOtbX+6P2VuBSN1MWhkM3GkbS44C\n8Pa6g392H88KmPQAH3WIyK83n2LwUNtCepA53JkCgYEAkf6f/yaaOcPpVHH09X9k\nMudrAvudHRIc8KULiB1s0hElHOMFL24LSwpU6CEiDIWmnV0QcAoMPDYG2etQyHYq\n1fwECBDd3qjTOd7J6ZXIbq2Hx0uwEK7aV+YWyte64ECGTNW2k7LX91mUAGpiuGLb\nWg64pnQV9/0z3y28aId82+k=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-tgdz6@cs3219-8da08.iam.gserviceaccount.com",
+  "client_id": "108712136279539758871",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-tgdz6%40cs3219-8da08.iam.gserviceaccount.com"
+}
